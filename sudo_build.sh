52affd96772163d6d30201d9c5358adbb549e879
Keenan Falcon <keenan.sogaard@gmail.com>
2025-03-12 12:10:55 +0100

    Add script for convenience

---
+++ sudo_build.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+PARAM=${1:-all}
+
+export TRUENAS_VERSION=25.04-RC.2-Custom
+export TRUENAS_TRAIN="TrueNAS-SCALE-Fangtooth"
+export SKIP_SOURCE_REPO_VALIDATION=0
+
+case "$PARAM" in
+    release)
+        make checkout
+        PACKAGES=kernel PARALLEL_BUILDS=1 make packages
+        PACKAGES=kernel-dbg PARALLEL_BUILDS=1 make packages
+        make
+    ;;
+    all)
+        make
+    ;;
+    checkout)
+        make checkout
+    ;;
+    packages)
+        make packages
+    ;;
+    update)
+        make update
+    ;;
+    iso)
+        make iso
+    ;;
+    clean)
+        make clean
+    ;;
+    *)
+        echo >&2 "Invalid option: $PARAM"
+        exit 1
+    ;;
+esac
